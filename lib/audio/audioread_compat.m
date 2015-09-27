7c40e3834d39edddb894b599e6a1bf738594c373
Joakim Andén <janden@math.princeton.edu>
2015-09-26 20:11:42 -0400

    Make audioread_compat compatible with Octave
    Octave uses different identifiers for exceptions, so this needs
    to be taken into account. Also, the wavread implementation does
    not implement non-double outputs, so an error is issued in this
    circumstance.

--- lib/audio/audioread_compat.m
+++ lib/audio/audioread_compat.m
@@ -5,7 +5,8 @@ switch nargin
         try
             [y,Fs] = audioread(file_path);
         catch message;
-            if strcmp(message,'MATLAB:UndefinedFunction')
+            if strcmp(message,'MATLAB:UndefinedFunction') || ...
+                    strcmp(message.identifier, 'Octave:undefined-function')
                 [y,Fs] = wavread(file_path);
             else
                 rethrow(message);
@@ -17,6 +18,15 @@ switch nargin
         catch message;
             if strcmp(message,'MATLAB:UndefinedFunction')
                 [y,Fs] = wavread(file_path,samples);
+            elseif strcmp(message.identifier, 'Octave:undefined-function')
+                if isnumeric(samples)
+                    [y,Fs] = wavread(file_path,samples);
+                elseif ischar(samples) && strcmp(samples, 'double')
+                    [y,Fs] = wavread(file_path);
+                else
+                    error(['Octave only supports calling wavread for' ...
+                        ' ''double'' datatype.']);
+                end
             else
                 rethrow(message);
             end
@@ -27,6 +37,13 @@ switch nargin
         catch message;
             if strcmp(message,'MATLAB:UndefinedFunction')
                 [y,Fs] = wavread(file_path,samples,dataType);
+            elseif strcmp(message.identifier, 'Octave:undefined-function')
+                if strcmp(dataType, 'double')
+                    [y,Fs] = wavread(file_path,samples);
+                else
+                    error(['Octave only supports calling wavread for' ...
+                        ' ''double'' datatype.']);
+                end
             else
                 rethrow(message);
             end
